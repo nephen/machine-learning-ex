44a95bb57fdfd14e8fd4d09eee5d41dbf2fd71af
Lever <995168694@qq.com>
2018-12-11 18:44:28 +0800

    add gradientDescent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -18,9 +18,12 @@ for iter = 1:num_iters
     %
 
 
-
-
-
+    temp1 = theta(1) - alpha*sum(X*theta-y)/m;
+    temp2 = theta(2) - alpha*sum((X*theta-y).*X(:,2))/m;
+    
+    % update theta0 and theta1 simultaneously
+    theta(1) = temp1;
+    theta(2) = temp2;
 
 
     % ============================================================
